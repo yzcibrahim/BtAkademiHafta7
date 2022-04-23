efc9554c524e9cc7a36eecc684f10f37b90ee401
ibrahim <yzcibrahim@gmail.com>
2022-04-23 13:59:54 +0300

    Add project files.

---
+++ 07_01_Dependency_Injection/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _07_01_Dependency_Injection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //A instanceA = new A();
+            ////instanceA = null;
+            //A instanceA2 = new A();
+            //method1DI();
+            Context ctx = new Context();
+
+            Repository rep1 = new Repository(ctx);
+            rep1.SaveRepo();
+
+            Repository rep2 = new Repository(ctx);
+            rep2.SaveRepo();
+
+            
+
+            
+
+        }
+
+        private static void method1DI()
+        {
+            B b = new B();
+            A instanceA = new A(b);
+            A instanceA2 = new A(b);
+            instanceA = null;
+            instanceA2 = null;
+        }
+
+
+    }
+
+    public class Repository
+    {
+        
+        Context _context;
+        public Repository(Context ctx)
+        {
+            _context = ctx;
+           // _context = new Context();
+        }
+
+        public void SaveRepo()
+        {
+            _context.Kaydet();
+        }
+    }
+
+    public class Context
+    {
+        public void Kaydet()
+        {
+            Console.WriteLine("Mssql Db ye lkayıt yapıldı");
+        }
+    }
+
+    public class A
+    {
+        B _b;
+        public A(B b)
+        {
+            //_b = new B();
+            _b = b;
+           
+        }
+        
+    }
+
+    public class B
+    {
+        
+    }
+}
---
+++ 07_02_singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07_02_singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = A.CreateA();
+            a1.Name = "birinci A";
+
+            A a2 = A.CreateA();
+
+
+            A a3 = A.CreateA();
+        }
+    }
+
+    public class A
+    {
+        public static A Ainstance;
+
+        public string Name;
+        public static A CreateA()
+        {
+            lock (new object())
+            {
+                if (Ainstance == null)
+                {
+
+                    Ainstance = new A();
+                }
+            }
+            return Ainstance;
+        }
+
+    }
+}
